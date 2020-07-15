a1c6a86ededefc3f099cd321a82bc8d3406f58b0
tkosiorski <kosiorski.tomasz@gmail.com>
2020-07-15 12:04:29 +0200

    initial commit

---
+++ src/test/java/pl/kosiorski/multiplication/SocialMultiplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kosiorski.multiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMultiplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
