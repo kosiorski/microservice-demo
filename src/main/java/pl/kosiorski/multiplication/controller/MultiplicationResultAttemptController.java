7b30c117b9ef3ff4acb8e478ce3793f99cc56532
tkosiorski <kosiorski.tomasz@gmail.com>
2020-07-16 14:50:52 +0200

    Finished post method in MultiplicationResultAttemptController tests GREEN.

--- src/main/java/pl/kosiorski/multiplication/controller/MultiplicationResultAttemptController.java
+++ src/main/java/pl/kosiorski/multiplication/controller/MultiplicationResultAttemptController.java
@@ -3,8 +3,12 @@ package pl.kosiorski.multiplication.controller;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.kosiorski.multiplication.domain.MultiplicationResultAttempt;
 import pl.kosiorski.multiplication.service.MultiplicationService;
 
 @RestController
@@ -17,7 +21,12 @@ final class MultiplicationResultAttemptController {
     this.multiplicationService = multiplicationService;
   }
 
-  // TODO post
+  @PostMapping
+  ResponseEntity<ResultResponse> postResult(
+      @RequestBody MultiplicationResultAttempt multiplicationResultAttempt) {
+    return ResponseEntity.ok(
+        new ResultResponse(multiplicationService.checkAttempt(multiplicationResultAttempt)));
+  }
 
   @RequiredArgsConstructor
   @NoArgsConstructor(force = true)
