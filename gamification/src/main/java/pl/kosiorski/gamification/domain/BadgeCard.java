25ad14473136d9785043e7955a08b7cf60cd3477
tkosiorski <kosiorski.tomasz@gmail.com>
2020-07-22 14:40:52 +0200

    Added BadgeCard.

---
+++ gamification/src/main/java/pl/kosiorski/gamification/domain/BadgeCard.java
@@ -0,0 +1,36 @@
+package pl.kosiorski.gamification.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+@Entity
+public final class BadgeCard {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "BADGE_ID")
+  private final Long id;
+
+  private final Long userId;
+  private final long badgeTimestamp;
+  private final Badge badge;
+
+  public BadgeCard() {
+    this(null, null, 0, null);
+  }
+
+  public BadgeCard(final Long userId, final Badge badge) {
+    this(null, userId, System.currentTimeMillis(), badge);
+  }
+}
