2f397570f42b3a773bfc145d7995a22d8651bf12
tkosiorski <kosiorski.tomasz@gmail.com>
2020-07-22 14:36:46 +0200

    Working on gamification microservice. Added Badge enum.

---
+++ gamification/src/main/java/pl/kosiorski/gamification/domain/Badge.java
@@ -0,0 +1,11 @@
+package pl.kosiorski.gamification.domain;
+
+/** Enumeration with the different types of Badges that a user can win. */
+public enum Badge {
+  BRONZE_MULTIPLICATOR,
+  SILVER_MULTIPLICATOR,
+  GOLD_MULTIPLICATOR,
+
+  FIRST_ATTEMPT,
+  FIRST_WON
+}
