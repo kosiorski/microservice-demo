d81776b242302f4fa045944a01abd06573928480
tkosiorski <kosiorski.tomasz@gmail.com>
2020-07-28 11:38:12 +0200

    Wrote tests to LeaderBoardServiceImplTest RED.

--- gamification/src/main/java/pl/kosiorski/gamification/service/LeaderBoardServiceImpl.java
+++ gamification/src/main/java/pl/kosiorski/gamification/service/LeaderBoardServiceImpl.java
@@ -1,12 +1,20 @@
 package pl.kosiorski.gamification.service;
 
 import pl.kosiorski.gamification.domain.LeaderBoardRow;
+import pl.kosiorski.gamification.repository.ScoreCardRepository;
 
 import java.util.List;
 
 public class LeaderBoardServiceImpl implements LeaderBoardService {
-    @Override
-    public List<LeaderBoardRow> getCurrentLeaderBoard() {
+
+  private final ScoreCardRepository scoreCardRepository;
+
+  public LeaderBoardServiceImpl(ScoreCardRepository scoreCardRepository) {
+    this.scoreCardRepository = scoreCardRepository;
+  }
+
+  @Override
+  public List<LeaderBoardRow> getCurrentLeaderBoard() {
     return null;
-    }
+  }
 }
---
+++ gamification/src/test/java/pl/kosiorski/gamification/service/GameServiceImplTest.java
@@ -0,0 +1,4 @@
+package pl.kosiorski.gamification.service;
+
+public class GameServiceImplTest {
+}
