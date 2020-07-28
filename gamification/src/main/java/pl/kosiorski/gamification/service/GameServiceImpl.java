19c252ff87f0360fe7b1a585de9c99be2524fa63
tkosiorski <kosiorski.tomasz@gmail.com>
2020-07-28 15:46:29 +0200

    Wrote GameServiceImplTest RED.

--- gamification/src/main/java/pl/kosiorski/gamification/service/GameServiceImpl.java
+++ gamification/src/main/java/pl/kosiorski/gamification/service/GameServiceImpl.java
@@ -1,15 +1,27 @@
 package pl.kosiorski.gamification.service;
 
 import pl.kosiorski.gamification.domain.GameStats;
+import pl.kosiorski.gamification.repository.BadgeCardRepository;
+import pl.kosiorski.gamification.repository.ScoreCardRepository;
 
 public class GameServiceImpl implements GameService {
-    @Override
-    public GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct) {
-    return null;
-    }
 
-    @Override
-    public GameStats retrieveStatsForUser(Long userId) {
-        return null;
-    }
+  private final ScoreCardRepository scoreCardRepository;
+  private final BadgeCardRepository badgeCardRepository;
+
+  public GameServiceImpl(
+      ScoreCardRepository scoreCardRepository, BadgeCardRepository badgeCardRepository) {
+    this.scoreCardRepository = scoreCardRepository;
+    this.badgeCardRepository = badgeCardRepository;
+  }
+
+  @Override
+  public GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct) {
+    return null;
+  }
+
+  @Override
+  public GameStats retrieveStatsForUser(Long userId) {
+    return null;
+  }
 }
