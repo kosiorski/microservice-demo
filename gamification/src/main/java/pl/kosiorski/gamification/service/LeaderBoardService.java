6a0a8d4a45149fa9d67700af6d08d3d3394eaeb2
tomasz <kosiorski.tomasz@gmail.com>
2020-11-02 12:43:55 +0100

    Created GameService and LeaderBoardService interfaces.

---
+++ gamification/src/main/java/pl/kosiorski/gamification/service/LeaderBoardService.java
@@ -0,0 +1,16 @@
+package pl.kosiorski.gamification.service;
+
+import pl.kosiorski.gamification.domain.LeaderBoardRow;
+
+import java.util.List;
+
+/** Provides methods to access the LeaderBoard with users and scores. */
+public interface LeaderBoardService {
+
+  /**
+   * Retrieves the current leader board with the top score users
+   *
+   * @return the users with the highest score
+   */
+  List<LeaderBoardRow> getCurrentLeaderBoard();
+}
