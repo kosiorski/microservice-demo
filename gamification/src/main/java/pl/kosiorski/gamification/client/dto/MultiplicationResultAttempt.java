250703b0311a66c652cf3f80fe5715ce89c9a9df
tomasz <kosiorski.tomasz@gmail.com>
2020-11-04 14:46:40 +0100

    Added MultiplicationResultAttempt and MultiplicationResultAttemptDeserializer to gamification.

--- gamification/src/main/java/pl/kosiorski/gamification/client/dto/MultiplicationResultAttempt.java
+++ gamification/src/main/java/pl/kosiorski/gamification/client/dto/MultiplicationResultAttempt.java
@@ -1,4 +1,31 @@
 package pl.kosiorski.gamification.client.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import pl.kosiorski.gamification.client.MultiplicationResultAttemptDeserializer;
+
+/** Identifies the attempt from a user to solve a multiplication. */
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+@JsonDeserialize(using = MultiplicationResultAttemptDeserializer.class)
 public class MultiplicationResultAttempt {
+
+  private final String userAlias;
+  private final int multiplicationFactorA;
+  private final int multiplicationFactorB;
+  private final int resultAttempt;
+  private final boolean correct;
+
+  MultiplicationResultAttempt() {
+    userAlias = null;
+    multiplicationFactorA = -1;
+    multiplicationFactorB = -1;
+    resultAttempt = -1;
+    correct = false;
+  }
 }
